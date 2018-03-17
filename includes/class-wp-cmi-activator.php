f727c8c335f2a5cad5b6785f3aaa5cf4d9e16c7e
MR612 <me@mrashidi.com>
2018-03-17 21:57:34 +0330

    initial release

---
+++ includes/class-wp-cmi-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://mrashidi.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Cmi
+ * @subpackage Wp_Cmi/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Cmi
+ * @subpackage Wp_Cmi/includes
+ * @author     Mahdi Rashidi <mrashidi.com@gmail.com>
+ */
+class Wp_Cmi_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-cmi-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://mrashidi.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Cmi
+ * @subpackage Wp_Cmi/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Cmi
+ * @subpackage Wp_Cmi/includes
+ * @author     Mahdi Rashidi <mrashidi.com@gmail.com>
+ */
+class Wp_Cmi_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
